1fdfda6403f673181bb3f6f1f4e2f0e193c496ff
fitsum-kebede <fitsuminfome@gmail.com>
2023-06-08 08:46:10 -0700

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,6 @@
-#!/user/bin/python3
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
+    """Print an addition of all arguments."""
     import sys
 
     total = 0
