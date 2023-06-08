39761d1434b4359124243b6b8881891a60de71c9
fitsum-kebede <fitsuminfome@gmail.com>
2023-06-08 08:39:37 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
+
 if __name__ == "__main__":
-    if len(argv) == 1:
+    """Print the number and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif len(argv) == 2:
+    elif count == 1:
         print("1 argument:")
-        print("1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(len(argv) - 1))
-        for argument in range(1, len(argv)):
-            print("{}: {}".format(argument, argv[argument]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
