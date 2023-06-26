819517ca12d81842010a0edf2bf772c70a3f2983
fitsum-kebede <fitsuminfome@gmail.com>
2023-06-26 10:45:55 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    total = 0
-    for i in range(x):
-        try:
-            print(f"{my_list[i]}", end="")
-            total += 1
-        except IndexError:
-            break
-    print()
-    return(total)
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+        print("")
+    except IndexError:
+        print("")
+        return i
+    return x
