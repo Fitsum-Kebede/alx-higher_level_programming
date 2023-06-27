5db3751308361d3005a0077420c623142a545c51
fitsum-kebede <fitsuminfome@gmail.com>
2023-06-27 12:33:55 -0700

    0x06-python-classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """class that defines a square"""
 class Square:
-"""class Square that defines a square"""
+    """class Square that defines a square"""
     __size = None
 
     def __init__(self, size):
+
         self.__size = size
